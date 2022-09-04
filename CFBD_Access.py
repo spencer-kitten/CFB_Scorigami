77aceb0ac36df5e0b9f0c954bda61d97f55bde46
Spencer Kitten <69778435+spencer-kitten@users.noreply.github.com>
2022-09-04 08:49:28 -0700

    Add files via upload

---
+++ CFBD_Access.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  6 19:00:13 2021
+
+@author: spenc
+"""
+import cfbd 
+
+# Create configuration to interact with cfbd
+configuration = cfbd.Configuration()
+configuration.api_key['Authorization'] = 'yEoefkv11Eht+CxNyyu9PSwrgkBxkvldo1nUcuWT9FMz7FmG3skufO3dXTZ17jGq'
+configuration.api_key_prefix['Authorization'] = 'Bearer'
+#api_instance = cfbd.GamesApi(cfbd.ApiClient(configuration))
+
---
+++ Twitter_Access.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  1 23:22:09 2022
+
+@author: spencer
+"""
+
+
+API = 'CPGOMuDa6YNPI98D8ff7BkARB'
+API_S = '0VQGzXLyKpvSBPYMMGl9UFMzbb8KJEsRc2Yh3GdxN7H2MP04mw'
+Access = '1434935301835616257-8Er2OJwv50MYefeQzC61NF3FWkWFCo'
+Access_S = 'Tw1gK8o64oMhLDgt6V45UWi3w1RgGB501JzyxeygfENYH'
